676e78055a19c899f12d8d636f5bd1ab04662c1a
Soumojit <sanjugon2003@gmail.com>
2025-08-19 16:56:10 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -47,7 +47,14 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params):
             # Save best test score
             report[model_name] = test_model_score
 
-        return report
+        return report   
+
+    except Exception as e:
+        raise CustomException(e,sys)
     
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj) 
     except Exception as e:
         raise CustomException(e,sys)
