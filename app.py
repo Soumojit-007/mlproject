28cb4b42178482600476ed60cf1f44ee967eb1ec
Soumojit <sanjugon2003@gmail.com>
2025-08-19 19:33:15 +0530

    Change

--- app.py
+++ app.py
@@ -39,6 +39,5 @@ def predict_datapoint():
 
         return render_template('home.html', results=results[0])
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=False , threaded=True)
+    app.run(host="0.0.0.0", port=5001, debug=False, threaded=True)
